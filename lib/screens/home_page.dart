c75b8b9a8ceee5497f662ab8d44fbca5d4251230
Noegeider <noegeider4@gmail.com>
2025-10-09 16:32:39 -0600

    AddBar added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,12 +4,37 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _MyWidgetState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyWidgetState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0, // quita sombra
+        // Icono de la izquierda
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[800],
+        ),
+        // Icono de la derecha
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person, color: Colors.black),
+          ),
+        ],
+      ),
+      body: Column(
+        children: const [
+          // texto principal
+          // pestañas
+          // contenido de pestañas
+          // carrito
+        ],
+      ),
+    );
   }
 }
